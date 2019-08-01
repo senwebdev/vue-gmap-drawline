d1c7367ee55044016d30678e5050b71bb772a834
sr.weloper <sen.webdev@hotmail.com>
2019-08-01 00:24:13 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <TravelMap class="travel-map"/>
+  </div>
+</template>
+
+<script>
+import TravelMap from "./components/TravelMap";
+
+export default {
+  name: "App",
+  components: {
+    TravelMap
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.travel-map {
+  height: 400px;
+}
+</style>
+
